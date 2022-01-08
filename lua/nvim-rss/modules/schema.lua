18bdbb0a05555447a4836cf63d7b344cb7867728
Pritesh Tupe <priteshtupe@gmail.com>
2022-01-08 14:17:23 +0530

    Add schema file

---
+++ lua/nvim-rss/modules/schema.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+M.FEED_TABLE = "feeds"
+
+M.ENTRY_TABLE = "entries"
+
+M.FEED_SCHEMA = {
+  link = { -- feed url, also id in atom
+    "text",
+    primary = true,
+    unique = true,
+  },
+  title = {
+    "text",
+    required = true,
+  },
+  format = { -- is "atom" or "rss"
+    "text",
+    required = true,
+  },
+  favourite = "boolean",
+  subtitle = "text",
+  version = "text",
+  htmlUrl = "text",
+  rights = "text",
+  generator = "text",
+  author = "text",
+}
+
+M.ENTRY_SCHEMA = {
+  link = { -- item url
+    "text",
+    required = true,
+    unique = true,
+    primary = true,
+  },
+  title = {
+    "text",
+    required = true,
+  },
+  summary = "text",
+  updated = "text",
+  updated_parsed = "number",
+  feed = {
+    type = "text",
+    required = true,
+    reference = "feeds.link",
+  },
+  starred = "boolean",
+
+  fetched = "number", -- timestamp of when feed was fetched
+}
+
+return M
